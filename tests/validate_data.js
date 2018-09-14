435d7d7362cb8372ba30336c36190d87739848fd
Michael - Blurpesec <hahn.michael.f@gmail.com>
2018-09-14 12:09:57 -0400

    Change name

--- tests/validate_data.js
+++ tests/validate_data.js
@@ -3,19 +3,19 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 describe('YAML Validator', function() {
-	describe('scams.yaml', function() {
+	describe('urls.yaml', function() {
 		it('should contain valid YAML', function(){
-			assert.doesNotThrow(() => yaml.safeLoad(fs.readFileSync('./_data/scams.yaml', 'utf8')));
+			assert.doesNotThrow(() => yaml.safeLoad(fs.readFileSync('./_data/urls.yaml', 'utf8')));
 		});
 		it('every entry should have a url', function() {
-			assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/scams.yaml', 'utf8')).filter(entry => !('url' in entry)),[]);
+			assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/urls.yaml', 'utf8')).filter(entry => !('url' in entry)),[]);
 		});
 		it('every url should specify its protocol (http:// or https:// or mailto:)', function() {
-			assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/scams.yaml', 'utf8')).filter(entry => !entry.url.startsWith('http://') && !entry.url.startsWith('https://') && !entry.url.startsWith('mailto:')),[]);
+			assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/urls.yaml', 'utf8')).filter(entry => !entry.url.startsWith('http://') && !entry.url.startsWith('https://') && !entry.url.startsWith('mailto:')),[]);
 		});
 		it('every entry should have valid keys (url/category/subcategory/description/addresses)', function() {
-			/* BEFORE migrate.js */ assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/scams.yaml', 'utf8')).filter(entry => Object.keys(entry).some(key => !['null','id','name','coin','url','category','subcategory','description','addresses'].includes(key))),[]);
-			/* AFTER migrate.js */ /* assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/scams.yaml', 'utf8')).filter(entry => Object.keys(entry).some(key => !['coin','url','category','subcategory','description','addresses'].includes(key))),[]); */
+			/* BEFORE migrate.js */ assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/urls.yaml', 'utf8')).filter(entry => Object.keys(entry).some(key => !['null','id','name','coin','url','category','subcategory','description','addresses'].includes(key))),[]);
+			/* AFTER migrate.js */ /* assert.deepEqual(yaml.safeLoad(fs.readFileSync('./_data/urls.yaml', 'utf8')).filter(entry => Object.keys(entry).some(key => !['coin','url','category','subcategory','description','addresses'].includes(key))),[]); */
 		});
 	});
 	describe('legit_urls.yaml', function() {
